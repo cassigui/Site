44761c0021f571f8ed1312e1104a299881d26d77
cassigui <caguilherme14@gmail.com>
2024-03-20 17:36:54 -0300

    carousel

--- navbarScroll.js
+++ navbarScroll.js
@@ -17,8 +17,8 @@ navlinks.forEach(navLinkEl => {
 
 window.addEventListener("scroll", () => {
   let header = document.querySelector("#header")
-  let startSection = document.querySelector("#inicio")
-  let startPosition = startSection.offsetTop
+  let startSection = document.querySelector("#inicio").offsetTop
 
-  header.classList.toggle('scrolling', window.scrollY > startPosition + 600)
+  header.classList.toggle('scrolling', window.scrollY > startSection + 600)
 })
+
