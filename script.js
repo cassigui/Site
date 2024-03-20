05ee0e621756869df9c79d6f5569e3bc236512fb
cassigui <caguilherme14@gmail.com>
2024-03-19 21:53:49 -0300

    Ficando aceitavel

---
+++ script.js
@@ -0,0 +1,24 @@
+let id = window.location.hash.substring(1);
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (id == "") {
+    document.getElementById("inicio-link").classList.add("active");
+  }
+  document.getElementById(id + "-link").classList.add("active");
+})
+
+const navlinks = document.querySelectorAll('.nav-link')
+navlinks.forEach(navLinkEl => {
+  navLinkEl.addEventListener("click", () => {
+    document.querySelector('.active')?.classList.remove('active')
+    navLinkEl.classList.add("active")
+  })
+})
+
+window.addEventListener("scroll", () => {
+  let header = document.querySelector("#header")
+  let startSection = document.querySelector("#inicio")
+  let startPosition = startSection.offsetTop
+
+  header.classList.toggle('scrolling', window.scrollY > startPosition + 600)
+})
